5e1e1e5c9dd28dbed60c0afe7a794b7d414d4272
Thelordgreat <greatokafor15@gmail.com>
2022-08-26 15:39:50 +0100

    updates

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
+import { BsHouseDoor } from "react-icons/bs";
+import { FiPhoneCall } from "react-icons/fi";
+import { FaRegEnvelope } from "react-icons/fa";
+import vector from "../../Images/Vector.png";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section>
+      <div className="contact_container">
+        <h2>Contact Me</h2>
 
-export default Contact
+        <div className="contact_content">
+          <div className="contact_boxes">
+            <div className="box">
+              <BsHouseDoor className="icon" />
+              <p>insert address</p>
+            </div>
+            <div className="box">
+              <FiPhoneCall className="icon" />
+              <p>+123-456-789-00</p>
+            </div>
+            <div className="box">
+              <FaRegEnvelope className="icon" />
+              <p>example@gmail.com</p>
+            </div>
+          </div>
+
+          <div className="contact_form">
+            <form>
+              <div className="inputs">
+                <input type="text" placeholder="Enter Name" />
+                <input type="email" placeholder="Enter Email" />
+              </div>
+
+              <input
+                type="text"
+                placeholder="Type Message"
+                className="message"
+              />
+
+              <div className="button">
+                <a href="/" className="btn">
+                  Send Message
+                  <img src={vector} alt="" />
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
 import Hero from "../../Images/hero image 1.png";
-import { BsInstagram, BsTwitter , BsGithub } from "react-icons/bs"
+import { FiTwitter, FiGithub, FiInstagram } from "react-icons/fi";
 
 const Header = () => {
   return (
     <section>
-      <div className='hero_content'>
+      <div className="hero_content">
         <div className="hero_texts">
           <h4>Hello i'm</h4>
           <h1>Great Okafor</h1>
           <h4>Front-end Developer</h4>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsum ad ducimus blanditiis delectus inventore, minus sapiente nemo molestiae at ipsa ratione, maiores voluptate numquam dicta aliquid suscipit accusantium temporibus.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsum
+            ad ducimus blanditiis delectus inventore, minus sapiente nemo
+            molestiae at ipsa ratione, maiores voluptate numquam dicta aliquid
+            suscipit accusantium temporibus.
+          </p>
 
           <div>
             <h4>Find Me On</h4>
-            <div className='social_icons'>
-              <BsInstagram className='icon'/>
-              <BsTwitter className='icon'/>
-              <BsGithub className='icon'/>
+            <div className="social_icons">
+              <FiInstagram className="icon" />
+              <FiTwitter className="icon" />
+              <FiGithub className="icon" />
             </div>
           </div>
         </div>
 
-        <div className='hero_img_shadow'>
+        <div className="hero_img_shadow">
           <img src={Hero} alt="" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
