76c120899ac27ffa2458755fc6411b52cd3c957b
Thelordgreat <greatokafor15@gmail.com>
2022-09-25 01:42:54 +0100

    design test

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,8 @@ import { FiTwitter, FiGithub, FiInstagram } from "react-icons/fi";
 const Header = () => {
   return (
     <section id="header">
-      <div className="hero_content">
+      {/*=== desktop layout ===== */}
+      <div className="hero_content_desktop">
         <div className="hero_texts">
           <div>
             <span>Hello i'm</span>
@@ -33,6 +34,38 @@ const Header = () => {
 
         <div className="hero_img_shadow">
           <img src={Hero} alt="" />
+        </div>
+      </div>
+
+          {/*====== Mobile Layout =======*/}
+      <div className="hero_content_mobile">
+        <div className="text_container">
+          <div className="intro">
+            <span>Hello i'm</span>
+            <span>Great Okafor</span>
+            <span className="title">Front-end Developer</span>
+          </div>
+
+          <div className="hero_img_shadow">
+            <img src={Hero} alt="" />
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsum
+            ad ducimus blanditiis delectus inventore, minus sapiente nemo
+            molestiae at ipsa ratione, maiores voluptate numquam dicta aliquid
+            suscipit accusantium temporibus.
+          </p>
+
+          <div className="bottom">
+            <h4>Find Me On</h4>
+            <div className="social_icons">
+              <FiInstagram className="icon" />
+              <FiTwitter className="icon" />
+              <FiGithub className="icon" />
+            </div>
+          </div>
+
         </div>
       </div>
     </section>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Contact from "../Components/Contact/Contact";
-import Experience from "../Components/Experience/Experience";
+// import Contact from "../Components/Contact/Contact";
+// import Experience from "../Components/Experience/Experience";
 import Header from "../Components/Header/Header";
-import Portfolio from "../Components/Portfolio/Portfolio";
-import Skills from "../Components/Skills/Skills";
+// import Portfolio from "../Components/Portfolio/Portfolio";
+// import Skills from "../Components/Skills/Skills";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Portfolio />
+      {/* <Portfolio />
       <Experience />
       <Skills />
-      <Contact />
+      <Contact /> */}
     </>
   );
 };
