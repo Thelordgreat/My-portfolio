3fba11a6edb40b7e65e3575c8aa0a4c56a986b2e
Thelordgreat <greatokafor15@gmail.com>
2022-09-26 03:02:47 +0100

    üpdates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer/Footer";
+import { themeContext } from "./context";
+import { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
 
+  return (
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "#1e1e1e" : "",
+        color: darkMode ? "#ffffff" : "",
+      }}
+    >
+      <Navbar />
       <Router>
         <Routes>
           <Route path={"/"} element={<Home />} />
